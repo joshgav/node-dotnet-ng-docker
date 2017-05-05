834eb9806ab40c08f193d943f3732e413e696161
Josh Gavant <josh.gavant@outlook.com>
2017-05-05 14:37:23 -0700

    use integrated diagnostic-channel in AI

--- service-api/bootstrap.js
+++ service-api/bootstrap.js
@@ -2,14 +2,6 @@ const dotenv        = require('dotenv').config();
 const appInsights   = require('applicationinsights');
 const pkg           = require('./package.json');
 
-// to be removed once integrated in AI
-const diagChannel   = require('diagnostic-channel').channel;
-diagChannel.autoLoadPackages(__dirname);
-diagChannel.addContextPreservation((cb) => {
-  return Zone.current.wrap(cb, "AI-ContextPreservation");
-});
-// /end
-
 if (!process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
   process.exitCode = 2;
   throw new Error("Expected ai env var.");
